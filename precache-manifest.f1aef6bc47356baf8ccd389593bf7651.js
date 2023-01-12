edf51f0d941aa76d61dce6dadd31e23110fac7d0
Saranya Dwarampudi <saranyakarri@yahoo.com>
2023-01-12 17:41:51 +0530

    Updates

---
+++ precache-manifest.f1aef6bc47356baf8ccd389593bf7651.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92c91f4310ca4c2da249fa7ed0358126",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "97e313c4a6537352b882",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "c6d8986263ab1883b070",
+    "url": "/portfolio/static/css/main.0c5effdc.chunk.css"
+  },
+  {
+    "revision": "97e313c4a6537352b882",
+    "url": "/portfolio/static/js/2.4add9792.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.4add9792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d8986263ab1883b070",
+    "url": "/portfolio/static/js/main.f5491875.chunk.js"
+  },
+  {
+    "revision": "978f3ae5392d42319a65",
+    "url": "/portfolio/static/js/runtime-main.37c4fe7f.js"
+  }
+]);
